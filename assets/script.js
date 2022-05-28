464e23a0a08a188fc7856af04f2b5e41680571a7
YonisHussein <yonismhussein@gmail.com>
2022-05-27 21:04:12 -0400

    Javascript cont.

--- assets/script.js
+++ assets/script.js
@@ -19,9 +19,31 @@ var currentHour = moment().format("HH");
         for (let i = 0; i < timeBlock.length; i++) {
             var descElement = timeBlock[x].children[1];
             stringElement = timeBlock[x].id.toStringElement();
-            $(descElement).val(localStorage.getItem(stringElement);
+            $(descElement).val(localStorage.getItem(stringElement));
+        }
+
+        for (let i = 0; i < timeBlock.length; i++) {
+            schedule = timeBlock [i].id;
+
+            function taskLegend () {
+                if (schedule < currentHour) {
+                    ${timeBlock[i]}.addClass("past");
+                }
+                else if (schedule === currentHour) {
+                    $(timeBlock[i]).removeClass("past");
+                    $(timeBlock[i]).addClass("present");
+                }
+                else {
+                    $(timeBlock[i]).removeClass("past");
+                    $(timeBlock[i]).removeClass("present");
+                    $(timeBlock[i]).removeClass("future");
+                }
+            }
+        taskLegend();
         }
     }
+
+    
 
 
 
